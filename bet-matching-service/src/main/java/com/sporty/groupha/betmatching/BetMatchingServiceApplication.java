203cdfe7c95e61ae9d2a042c740304db50ff4af9
Marcos <marcosmoraisdev@gmail.com>
2026-03-05 22:17:51 +0000

    chore: bootstrap multi-module microservices workspace

---
+++ bet-matching-service/src/main/java/com/sporty/groupha/betmatching/BetMatchingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sporty.groupha.betmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetMatchingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetMatchingServiceApplication.class, args);
+    }
+}
